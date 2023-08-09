d6ad8d177bf74ca9838e54fb0f345f70a8a23ccd
shivramrca <46129761+shivramrca@users.noreply.github.com>
2023-08-09 18:50:06 +0800

    Update streamlit.app.py
    change ( to [ in the pandas.dataframe.loc command

--- streamlit.app.py
+++ streamlit.app.py
@@ -13,7 +13,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Lets put a picklist here so that users can pick the fruits of their choice
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc(fruits_selected)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on the list
 # streamlit.dataframe(my_fruit_list)
