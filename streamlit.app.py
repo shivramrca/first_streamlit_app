14a8c64b2ed259a3430c5857d380c4d25eaa2d61
shivramrca <46129761+shivramrca@users.noreply.github.com>
2023-08-09 18:47:54 +0800

    Update streamlit.app.py
    Show the table based on the selection

--- streamlit.app.py
+++ streamlit.app.py
@@ -12,8 +12,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Lets put a picklist here so that users can pick the fruits of their choice
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc(fruits_selected)
 
 # display the table on the list
-streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
